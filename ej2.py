0f02a4a5926ac7e83883bc8634dd3abe76fd612b
Nahuel Nievas <nievasnahuel@gmail.com>
2022-10-06 00:22:06 +0200

    final version

--- ej2.py
+++ ej2.py
@@ -1,28 +1,28 @@
-# add_time("3:00 PM", "3:10")
-# Returns: 6:10 PM
+def add_time (start, duration, day_week= False):
 
+    weekdays_index= {
+        "monday": 0,
+        "tuesday": 1,
+        "wednesday": 2,
+        "thursday": 3,
+        "friday": 4,
+        "saturday": 5,
+        "sunday": 6
 
-
-# def addtime (a):
-
-#     hour1= int(a.split(":")[0])
-#     minute1= int(a.split(":")[1])
-#     # hour2= b.split(":")[0]
-#     # hour2= b.split(":")[1]
+    }
     
-#     print(hour1)
-#     print(type(hour1))
-#     print(minute1)
-
-#     print(hora1)
-
- 
-
-
-
-
-def add_time (start, duration):
-    # start = "3:50 pm"
+     
+    
+    
+    weekdays_arr = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
 
     #start format separation
     time1= start.split()[0]
@@ -30,53 +30,74 @@ def add_time (start, duration):
     hour1 = int(time1.split(":")[0])
     minute1 = int(time1.split(":")[1])
 
-    print(time1)
-    print(pmam1)
-    print(hour1)
-    print(minute1)
+    # print(time1)
+    # print(pmam1)
+    # print(hour1)
+    # print(minute1)
+
+    if pmam1 == "PM":
+        hour1 += 12
 
    # duration format separation
-
     hour2 = int(duration.split(":")[0])
     minute2 = int(duration.split(":")[1])
-    print(hour2)
-    print(minute2)
+    # print(hour2)
+    # print(minute2)
 
+
+    #calculations
+    total_minutes = minute1 + minute2 
+    final_minutes = total_minutes % 60
+    extra_hours = total_minutes // 60
+    total_hour = hour1 + hour2 + extra_hours
+    # print(total_minutes)
+    # print(total_hour)
+    
 
     
-    total_minutes = minute1 + minute2 
-    print(total_minutes)
-    final_minutes = total_minutes % 60
-    print(final_minutes)
-    extra_hours = total_minutes // 60
-    print(extra_hours)
-    total_hour = hour1 + hour2 + extra_hours
-    print(total_hour)
-    # final_hour = hour1 + hour2
-    # final_minute= minute1 + minute2
-    # print(final_hour)
-    # print(final_minute)
-
-    # final_result = str(final_hour) + ":" + str(final_minute) + " " +pmam1
-    # print(final_result)
-    # print(type(final_result))
     
     final_hour = (total_hour % 24) % 12
+    
 
-    # if final_hour == 0:
-    #     final_hour = 12
-    #     final_hour = str(final_hour)
 
+    #hour 0
+    if final_hour == 0:
+        final_hour = 12
+        final_hour = str(final_hour)
+
+    #calculating days
+    total_day= (total_hour // 24)
+    # total_day = int (hour2 // 24)
+    
+    
+
+    # deciding mid day (AM/PM)
+    ampm_final = ""
+    if (total_hour % 24) <= 11:
+        ampm_final = "AM"
+    else:
+        ampm_final = "PM"
+ 
+    #minutes under 10
     if final_minutes <=9:
         final_minutes = "0" + str(final_minutes)
 
-    new_time = str(final_hour) + ":" + str(final_minutes) + " " + pmam1
-    print(new_time)
+    
     
 
-# add_time("3:00 PM", "3:10")
+    new_time = str(final_hour) + ":" + str(final_minutes) + " " + ampm_final
+    
+    if(day_week):
+         day_week = day_week.lower()
+         index = int((weekdays_index[day_week]) + total_day) % 7
+         new_day = weekdays_arr[index]
+         new_time += ", " + new_day
 
-add_time("11:55 AM", "3:12")
+    if(total_day == 1):
+       return new_time + " " + "(next day)"
+    elif (total_day > 1):
+         return new_time + " (" + str(total_day) + " days later)"
 
-
-
+    
+    return new_time
+    
